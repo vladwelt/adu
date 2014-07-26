8ec8a2ea2e20ee81eba9e1b619516a3790fc4677
vladwelt <vladwelt@gmail.com>
2014-07-26 04:20:32 -0400

    [ADD]Modelo inicial planteado

---
+++ modelo/Cliente.java
@@ -0,0 +1,90 @@
+package adu;
+
+class Cliente {
+
+    private int ci;//Carnet de identidad
+    private String nombre;
+    private String apellido_paterno;
+    private String apellido_materno;
+    private String direccion;
+    private int telefono_fijo;
+    private int telefono_celular;
+
+    public Cliente(int _ci, String _nombre, String _ap, String _am,
+            String _direccion, int _tf, int _tc) {
+        this.ci = _ci;
+        this.nombre = _nombre;
+        this.apellido_paterno = _ap;
+        this.apellido_materno = _am;
+        this.direccion = _direccion;
+        this.telefono_fijo = _tf;
+        this.telefono_celular = _tc;
+    }
+
+    public Cliente(int _ci, String _nombre, String _ap, String _am,
+            String _direccion, int _tc) {
+        Cliente(_ci, _nombre, _ap, _am, _direccion,0, _tc);
+    }
+
+    public Cliente(int _ci, String _nombre, String _ap,
+            String _direccion, int _tc) {
+        Cliente(_ci, _nombre, _ap, "", _direccion,0, _tc);
+    }
+    
+    public int getCi() {
+        return this.ci;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return this.apellido_paterno;
+    }
+
+    public String getApellidoMaterno() {
+        return this.apellido_materno;
+    }
+
+    public String getDireccion() {
+        return this.direccion;
+    }
+
+    public int getTelefonoFijo() {
+        return this.telefono_fijo;
+    }
+
+    public int getTelefonoCelular() {
+        return this.telefono_celular;
+    }
+
+    public void setCi(int _ci) {
+        this.ci = _ci;
+    }
+
+    public void setNombre(String _nombre) {
+        this.nombre = _nombre;
+    }
+
+    public void setApellidoPaterno(String _ap) {
+        this.apellido_paterno = _ap;
+    }
+
+    public void setApellidoMaterno(String _am) {
+        this.apellido_materno = _am;
+    }
+
+    public void setDireccion(String _direccion) {
+        this.direccion = _direccion;
+    }
+
+    public void setTelefonoFijo(int _tf) {
+        this.telefono_fijo = _tf;
+    }
+
+    public void setTelefonoCelular(int _tc) {
+        this.telefono_celular = _tc;
+    }
+
+}
---
+++ modelo/Lote.java
@@ -0,0 +1,49 @@
+package edu;
+
+class Lote {
+
+    private int id;
+    private double largo;
+    private double ancho;
+    private double precio;
+
+    public Lote(int _id, double _largo, double _ancho,
+            double precio) {
+        this.id = _id;
+        this.largo = _largo;
+        this.ancho = _ancho;
+        this.precio = _precio;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public double getLargo() {
+        return this.id;
+    }
+
+    public double getAncho() {
+        return this.id;
+    }
+
+    public double getPrecio() {
+        return this.id;
+    }
+
+    public void setId(int _id) {
+        this.id = _id;
+    }
+
+    public void setLargo(double _largo) {
+        this.largo = _largo;
+    }
+
+    public void setAncho(double _ancho) {
+        this.ancho = _ancho;
+    }
+
+    public void setPrecio(double _precio) {
+        this.precio = _precio;
+    }
+}
