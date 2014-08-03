539d23bc5fd71f28374b9b1b31c62f59e6227542
jmejia <cromeocromeo@gmail.com>
2014-08-03 14:39:41 -0400

    agregando tema de skin con substance

--- src/adu/Main.java
+++ src/adu/Main.java
@@ -3,12 +3,16 @@ package adu;
 import adu.controlador.*;
 import adu.vista.*;
 import javax.swing.*;
+import org.jvnet.substance.SubstanceLookAndFeel;
 
 public class Main {
+
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable(){
+        JFrame.setDefaultLookAndFeelDecorated(false);
+        SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                UIManager.put("swing.boldMetal",Boolean.FALSE);
+                SubstanceLookAndFeel.setSkin("org.jvnet.substance.skin.CremeSkin");
+//                UIManager.put("swing.boldMetal",Boolean.FALSE);
                 VistaAdu adu = new VistaAdu("Administrador de Urbanizaciones");
                 Controlador cont = new Controlador(adu);
                 adu.runTest();
